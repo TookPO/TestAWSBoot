4ff62eb7a8f6b5bcbec553376685fb77a8028e22
bavsa <bavsa@192.168.219.143>
2022-01-26 14:11:32 +0900

    gitTest

---
+++ TestAWSBoot/src/main/java/com/legacy/post/TestAwsBootApplication.java
@@ -0,0 +1,13 @@
+package com.legacy.post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAwsBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAwsBootApplication.class, args);
+	}
+
+}
---
+++ TestAWSBoot/src/test/java/com/legacy/post/TestAwsBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legacy.post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAwsBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
